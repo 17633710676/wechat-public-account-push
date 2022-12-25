5db8902f87f966326fdd22faf58fe2f983ca1fd5
17633710676 <121337511+17633710676@users.noreply.github.com>
2022-12-25 21:13:10 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -48,38 +48,38 @@ const USER_CONFIG = {
       ],
     },
   ],
-  USERS: [
-    {
-      // 想要发送的人的名字
-      name: '4CHEN',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o9xJ85nuXjEMkAe1ROFX6LB_QYFQ',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '1bGuB6uWnV-n-qCxUgoHnepAWE2Rw0VOpnjjfzI90to',
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      //horoscopeDate: '04-16',
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: 'bb', year: '1998', date: '11-01',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '*生日', name: 'cc', year: '2000', date: '03-12',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2023', date: '01-01',
-        },
-      ],
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2023-01-01' },
-        // 结婚纪念日
-        // { keyword: 'marry_day', date: '2022-09-09' },
-      ],
-    },
-  ],
+//   USERS: [
+//     {
+//       // 想要发送的人的名字
+//       name: '4CHEN',
+//       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+//       id: 'o9xJ85nuXjEMkAe1ROFX6LB_QYFQ',
+//       // 使用微信测试号：你想对他发送的模板消息的模板ID
+//       useTemplateId: '1bGuB6uWnV-n-qCxUgoHnepAWE2Rw0VOpnjjfzI90to',
+//       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+//       //horoscopeDate: '04-16',
+//       festivals: [
+//         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+//         {
+//           type: '*生日', name: 'bb', year: '1998', date: '11-01',
+//         },
+//         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+//         {
+//           type: '*生日', name: 'cc', year: '2000', date: '03-12',
+//         },
+//         {
+//           type: '节日', name: '相识纪念日', year: '2023', date: '01-01',
+//         },
+//       ],
+//       // 我们在一起已经有xxxx天了的配置
+//       customizedDateList: [
+//         // 在一起的日子
+//         { keyword: 'love_day', date: '2023-01-01' },
+//         // 结婚纪念日
+//         // { keyword: 'marry_day', date: '2022-09-09' },
+//       ],
+//     },
+//   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
