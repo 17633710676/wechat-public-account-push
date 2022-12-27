e0967e5c9822a1d91d2f51bb50a4ddcd1fd45576
17633710676 <121337511+17633710676@users.noreply.github.com>
2022-12-27 15:08:53 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,7 +19,7 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '4CHEN',
+      name: '4CH3N',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o9xJ85kgFhrBz5fRZgZQ9wRkM1uw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
